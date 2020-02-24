6190a634a11f0afa717224acf96bd2d20c31e562
WY1479052860 <1479052860@qq.com>
2020-02-24 13:12:10 +0800

    这是最近提交的数据

--- app/src/main/java/com/bawei/wangyi20200224/activity/MainActivity.java
+++ app/src/main/java/com/bawei/wangyi20200224/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.bawei.wangyi20200224;
+package com.bawei.wangyi20200224.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.bawei.wangyi20200224.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/bawei/wangyi20200224/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.bawei.wangyi20200224.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bawei.wangyi20200224.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
