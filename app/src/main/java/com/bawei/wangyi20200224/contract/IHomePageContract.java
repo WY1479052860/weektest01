0202111f5d83bdc38802c34243c47e1cbb2a4d0b
WY1479052860 <1479052860@qq.com>
2020-02-24 13:33:29 +0800

    这是最近的契约类

--- app/src/main/java/com/bawei/wangyi20200224/contract/IHomePageContract.java
+++ app/src/main/java/com/bawei/wangyi20200224/contract/IHomePageContract.java
@@ -1,10 +1,38 @@
 package com.bawei.wangyi20200224.contract;
 
 /**
- * <p>文件描述：<p>
- * <p>作者：DELL<p>
- * <p>创建时间：2020/1/9<p>
- * <p>版本号：1<p>
+ * 契约类
  */
 public interface IHomePageContract {
+    //v层
+    interface IView{
+        void getBannerSuccess(String str);
+        void getBannerFaliure(String str);
+
+        void getListSuccess(String str);
+        void getListFaliure(String str);
+    }
+    //p层
+    interface IPresenter{
+        void getBanner(String url);
+        void getList(String url);
+    }
+    //m层
+    interface IModel{
+        void getBanner(String url,ICallBackModel iCallBackModel);
+
+        interface ICallBackModel{
+            void getBannerSuccess(String str);
+            void getBannerFaliure(String str);
+        }
+        void getList(String url,CallBack callBack);
+
+        interface CallBack{
+            void getListSuccess(String str);
+            void getListFaliure(String str);
+        }
+
+
+    }
+
 }
